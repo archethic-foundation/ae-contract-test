9655ed101f9337217cefd929cdb4cfc66df51e02
Neylix <julien.leclerc05@protonmail.com>
2024-12-27 16:10:37 +0100

    Update archethic sdk version

--- deployment.ts
+++ deployment.ts
@@ -23,8 +23,8 @@ type TokenTransfer = {
 
 type Recipient = {
   to: string;
-  action?: string;
-  args?: any[]
+  action: string;
+  args?: object;
 }
 
 export type DeployTxDataOpt = {
@@ -55,30 +55,31 @@ export async function getDeployContractTx(account: Account, opts: DeployOpts = {
 
   let txData: TransactionData | undefined = undefined;
   if (opts.additionalData) {
-    txData = { content: "", ledger: { uco: {transfers: []}, token: { transfers: []}}, recipients: [], ownerships: []}
+    txData = { content: "", ledger: { uco: { transfers: [] }, token: { transfers: [] } }, recipients: [], ownerships: [] }
     if (opts.additionalData.content) {
       txData.content = opts.additionalData.content
     }
 
     if (opts.additionalData.ucoTransfers) {
       txData.ledger.uco.transfers = opts.additionalData.ucoTransfers.map((t) => {
-        return { to: hexToUint8Array(t.to), amount: t.amount}
-     })
+        return { to: hexToUint8Array(t.to), amount: t.amount }
+      })
     }
 
     if (opts.additionalData.tokenTransfers) {
       txData.ledger.token.transfers = opts.additionalData.tokenTransfers.map((t) => {
-        return { to: hexToUint8Array(t.to), amount: t.amount, tokenAddress: hexToUint8Array(t.tokenAddress), tokenId: t.tokenId}
-     })
+        return { to: hexToUint8Array(t.to), amount: t.amount, tokenAddress: hexToUint8Array(t.tokenAddress), tokenId: t.tokenId }
+      })
     }
 
     if (opts.additionalData?.recipients) {
       txData.recipients = opts.additionalData.recipients.map(r => {
+        if (!r.args) r.args = {}
         return { address: hexToUint8Array(r.to), action: r.action, args: r.args }
       })
     }
   }
- 
+
   let tx = await newContractTransaction(account.archethic, new Contract(bytecode, manifest), account.seed as string, txData)
 
   if (opts.additionalData?.ucoTransfers) {
@@ -108,7 +109,7 @@ export function getUpgradeContractTx(account: Account, contractAddress: string, 
   }
 
   const tx = updateContractTransaction(account.archethic, contractAddress, new Contract(bytecode, manifest))
-  
+
   if (opts.additionalData?.content) {
     tx.setContent(opts.additionalData?.content)
   }
@@ -126,15 +127,4 @@ export function getUpgradeContractTx(account: Account, contractAddress: string, 
   }
 
   return tx
-}
-
-async function compress(bytes: Buffer): Promise<Buffer> {
-  return new Promise((resolve, reject) => {
-    zlib.deflateRaw(bytes, (err, res) => {
-      if (err) {
-        return reject(err)
-      }
-      resolve(res)
-    })
-  })
 }
